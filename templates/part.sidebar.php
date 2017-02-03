b27057ec7a4783800465ffc2dbe952a5a37a4c6d
raceface2nd <raceface_the_one@gmx.net>
2017-02-02 22:44:06 -0800

    Revert "fixes"
    This reverts commit a3f5849f425627eab1d5a42dfd3b2cf10ad118fc.

--- templates/part.sidebar.php
+++ templates/part.sidebar.php
@@ -10,8 +10,8 @@
     <div id="sidebar-tab">
         <span id="sidebar_tab_1" class="sidebar_tab_active"><?php p($l->t('Share'));?></span>
         <span id="sidebar_tab_2"><?php p($l->t('Export'));?></span>
-        <span id="sidebar_tab_3"><?php p($l->t('Coloring'));?></span>
-        <span id="sidebar_tab_4"><?php p($l->t('Settings'));?></span>
+        <span id="sidebar_tab_3"><?php p($l->t('Settings'));?></span>
+        <span id="sidebar_tab_4"><?php p($1->t('Coloring'));?></span>
     </div>
     <div id="sidebar-content">
         <div id="sidebar_content_1" style="display: block">
@@ -21,11 +21,12 @@
             <?php print_unescaped($this->inc('part.sidebar.export')); ?>
         </div>
         <div id="sidebar_content_3">
-            <?php print_unescaped($this->inc('part.sidebar.coloring')); ?>
-        </div>
-        <div id="sidebar_content_4">
             <?php print_unescaped($this->inc('part.sidebar.settings')); ?>
         </div>
+        <div id="sidebar_content_4">
+            <?php print_unescaped($this->inc('part.sidebar.coloring')); ?>
+        </div>
+
     </div>
 </div>
 
