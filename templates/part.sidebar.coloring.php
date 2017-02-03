e01447dee0abb3996aff40e3d7cccfc1c36f3b60
raceface2nd <raceface_the_one@gmx.net>
2017-02-02 22:43:56 -0800

    Revert "template update"
    This reverts commit e3d190266881c32ad841e5ce13ab4da587a69aab.

--- templates/part.sidebar.coloring.php
+++ templates/part.sidebar.coloring.php
@@ -1,20 +1,5 @@
 <div id="chart_coloring" class="">
 
 this is a test
-    <div id="colorPicker">
-        <a class="color"><div class="colorInner"></div></a>
-        <div class="track"></div>
-        <ul class="dropdown"><li></li></ul>
-        <input type="hidden" class="colorInput"/>
-    </div>
-
-a second test
-    <div id="colorPicker">
-        <a class="color"><div class="colorInner"></div></a>
-        <div class="track"></div>
-        <ul class="dropdown"><li></li></ul>
-        <input type="hidden" class="colorInput"/>
-    </div>
-
 
 </div>
