8ba5b5da4ce846bc0839457a88711b697f666a0f
raceface2nd <raceface_the_one@gmx.net>
2017-02-09 12:45:40 -0800

    update

--- js/application/action/changecolor.js
+++ js/application/action/changecolor.js
@@ -19,7 +19,7 @@ if(App.namespace) { App.namespace('Action.Changecolor', function(App) {
 				var _tableRef = _table.getElementsByTagName('tbody')[0];
 
 				for(var groupName in groupsusers){
-					var _lineGroup = document.createElement('div'),
+/*					var _lineGroup = document.createElement('div'),
 					    _lineUsers = document.createElement('div'),
 					    _inputGroup = document.createElement('input'),
 					    _inputLabel = document.createElement('label'),
@@ -36,14 +36,15 @@ if(App.namespace) { App.namespace('Action.Changecolor', function(App) {
 					_inputGroup.id = 'group' + groupName;
 					_inputLabel.setAttribute('for', 'group' + groupName);
 					_inputLabel.innerHTML += ' <strong>' + Util.ucfirst(groupName) + '</strong>';
-					var rowCount = _tableRef.rows.length;
+*/					var rowCount = _tableRef.rows.length;
 					var row = _tableRef.insertRow(rowCount);
+					var colorId = 'col_g_' + groupName;
 					row.insertCell(0).innerHTML = '<b><i>' + Util.ucfirst(groupName) + '</i></b>';
-					row.insertCell(1).innerHTML = '1';
+					row.insertCell(1).innerHTML = '<input type="text" id="' + colorId +'" />';
 
 					for(var i=0; i<usersCount; i++){
 						if (deprecatedUsers.indexOf(users[i]['uid']) !== -1) continue;
-						var _inlineUser = document.createElement('span'),
+/*						var _inlineUser = document.createElement('span'),
 						    _inputUser = document.createElement('input'),
 						    _inputUserLabel = document.createElement('label'),
 						    _inputUserSpan = document.createElement('span');
@@ -59,10 +60,10 @@ if(App.namespace) { App.namespace('Action.Changecolor', function(App) {
 						_inlineUser.appendChild(_inputUser);
 						_inlineUser.appendChild(_inputUserLabel);
 						_lineUsers.appendChild(_inlineUser);
-						var rowCount = _tableRef.rows.length;
+*/						var rowCount = _tableRef.rows.length;
 						var row = _tableRef.insertRow(rowCount);
 						var uid = users[i]['uid'];
-						var colorId = 'col_' + uid;
+						var colorId = 'col_u_' + uid;
 						row.insertCell(0).innerHTML = uid;
 						row.insertCell(1).innerHTML = '<input type="text" id="' + colorId +'" />';
 					}
@@ -74,10 +75,31 @@ if(App.namespace) { App.namespace('Action.Changecolor', function(App) {
 				var users = groupsusers[groupName],
 				    usersCount =  users.length;
 
+					$("#col_g_" + uid).spectrum({
+		            color: "rgb(244, 204, 204)",    
+		            showPaletteOnly: true,
+		            palette: [
+		                ["rgb(0, 0, 0)", "rgb(67, 67, 67)", "rgb(102, 102, 102)",
+		                "rgb(204, 204, 204)", "rgb(217, 217, 217)","rgb(255, 255, 255)"],
+		                ["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
+		                "rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"], 
+		                ["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)", 
+		                "rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)", 
+		                "rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)", 
+		                "rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)", 
+		                "rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)", 
+		                "rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)",
+		                "rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)",
+		                "rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)",
+		                "rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)", 
+		                "rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
+		                ]
+		         });
+
 				for(var i=0; i<usersCount; i++){
 					if (deprecatedUsers.indexOf(users[i]['uid']) !== -1) continue;
 					var uid = users[i]['uid'];
-		        $("#col_" + uid).spectrum({
+		        $("#col_u_" + uid).spectrum({
 		            color: "rgb(244, 204, 204)",    
 		            showPaletteOnly: true,
 		            palette: [
