489dbb1355c7bf084971c295af2071421e917c63
raceface2nd <raceface_the_one@gmx.net>
2017-03-28 13:41:18 +0200

    Revert "added function"
    This reverts commit 27a526f07d4a3dbd37d8f74fdf6550a0cfd972fa.

--- js/application/action/changecolor.js
+++ js/application/action/changecolor.js
@@ -6,7 +6,7 @@ if (App.namespace) {
 			'use strict';
 
 			$(document).ready(function () {
-
+				
 				var Usercolors = function (baseUrl) {
 					this._baseUrl = baseUrl;
 					this._usercolors = [];
@@ -99,14 +99,35 @@ if (App.namespace) {
 						}
 					}
 
+					for (var groupName in groupsusers) {
+						var users = groupsusers[groupName],
+							usersCount = users.length;
 
-					renderColors: function() {
-						for (var groupName in groupsusers) {
-							var users = groupsusers[groupName],
-								usersCount = users.length;
-							
+						$("#col_g_" + groupName).spectrum({
+							color: "rgb(244, 204, 204)",
+							showPaletteOnly: true,
+							palette: [
+								["rgb(0, 0, 0)", "rgb(67, 67, 67)", "rgb(102, 102, 102)",
+									"rgb(204, 204, 204)", "rgb(217, 217, 217)", "rgb(255, 255, 255)"],
+								["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
+									"rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"],
+								["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)",
+									"rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)",
+									"rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)",
+									"rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)",
+									"rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)",
+									"rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)",
+									"rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)",
+									"rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)",
+									"rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)",
+									"rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
+							]
+						});
 
-							$("#col_g_" + groupName).spectrum({
+						for (var i = 0; i < usersCount; i++) {
+							if (deprecatedUsers.indexOf(users[i]['uid']) !== -1) continue;
+							var uid = users[i]['uid'];
+							$("#col_u_" + uid).spectrum({
 								color: "rgb(244, 204, 204)",
 								showPaletteOnly: true,
 								palette: [
@@ -126,62 +147,34 @@ if (App.namespace) {
 										"rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
 								]
 							});
-
-							for (var i = 0; i < usersCount; i++) {
-								if (deprecatedUsers.indexOf(users[i]['uid']) !== -1) continue;
-								var uid = users[i]['uid'];
-								var cindex = usercolors._usercolors.indexOf(uid);
-								console.log(cindex);
-								$("#col_u_" + uid).spectrum({
-									color: "rgb(244, 204, 204)",
-									showPaletteOnly: true,
-									palette: [
-										["rgb(0, 0, 0)", "rgb(67, 67, 67)", "rgb(102, 102, 102)",
-											"rgb(204, 204, 204)", "rgb(217, 217, 217)", "rgb(255, 255, 255)"],
-										["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
-											"rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"],
-										["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)",
-											"rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)",
-											"rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)",
-											"rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)",
-											"rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)",
-											"rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)",
-											"rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)",
-											"rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)",
-											"rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)",
-											"rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
-									]
-								});
-							}
 						}
-					});
-			}
+					}
+				});
 
 				var baseUrl = OC.generateUrl('apps/owncollab_chart');
-			var usercolor = {
-				user: 'new user',
-				colorcode: 'rgb(255, 255, 255)'
-			};
+				var usercolor = {
+					user: 'new user',
+					colorcode: 'rgb(255, 255, 255)'
+				};
 
-			var usercolors = new Usercolors(OC.generateUrl('/apps/owncollab_chart/colors'));
-			usercolors.loadAll().done(function () {
-				renderColors();
-				$.each(usercolors._usercolors, function (i, data) {
-					console.log('username: ' + data.user + ' color: ' + data.colorcode);
+				var usercolors = new Usercolors(OC.generateUrl('/apps/owncollab_chart/colors'));
+				usercolors.loadAll().done(function () {
+					$.each(usercolors._usercolors, function(i, data){
+						console.log('username: ' + data.user + ' color: ' + data.colorcode);
+					});
+				}).fail(function () {
+					alert('Could not load usercolors');
 				});
-			}).fail(function () {
-				alert('Could not load usercolors');
+//				console.log(baseUrl);
+//				$.getJSON(baseUrl + '/colors', function(result){
+//					var items="";
+//					$.each(result, function(i, data){
+//						console.log('uid:' + data.user + ' color:' + data.colorcode)
+//					});
+//				});
+
+
 			});
-			//				console.log(baseUrl);
-			//				$.getJSON(baseUrl + '/colors', function(result){
-			//					var items="";
-			//					$.each(result, function(i, data){
-			//						console.log('uid:' + data.user + ' color:' + data.colorcode)
-			//					});
-			//				});
-
-
-		});
-	})(OC, window, jQuery);
-})
+		})(OC, window, jQuery);
+	})
 }
