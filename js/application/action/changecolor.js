e9f91ae80c1e00641bcd8f43c6216a01263ddcb0
raceface2nd <raceface_the_one@gmx.net>
2017-03-28 15:33:19 +0200

    test

--- js/application/action/changecolor.js
+++ js/application/action/changecolor.js
@@ -112,7 +112,7 @@ if (App.namespace) {
 							var uid = users[i]['uid'];
 							var arr = usercolors._usercolors;
 							console.log(uid);
-							var j = arr.user.indexOf(uid);
+							var j = indexOf(arr[i]['uid']);
 							
 							console.log(j);
 							if (j >= 0){
